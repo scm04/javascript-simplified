a138fac1a8818124fc6b9f7ba0f1b09be68ed3a4
Spencer <spencer.meredith4@gmail.com>
2023-05-26 17:13:23 -0600

    Work for 26 May 2023
    - Add the year selector and get the base feature working.

--- beginner-course/Section 10 - Modules and Bundlers/73-date-picker/typescript-date-picker/src/main.ts
+++ beginner-course/Section 10 - Modules and Bundlers/73-date-picker/typescript-date-picker/src/main.ts
@@ -8,7 +8,9 @@ import {
 	startOfDay,
 	isEqual,
 	addMonths,
-	setMonth
+	setMonth,
+	setYear,
+	getYear
 } from "date-fns"
 
 // 1. On load: populate the date picker button with the current date.
@@ -46,13 +48,13 @@ function populateDatePicker() {
 	const selectedDate = startOfDay(new Date(datePickerToggle.dataset.selectedDate!))
 	const dateToShow = startOfDay(new Date(datePicker.dataset.dateToShow!))
 
-	updateCurrentMonth(dateToShow)
+	updateDisplayMonthAndYear(dateToShow)
 	populateDateGrid(dateToShow, selectedDate)
 }
 
 const displayMonth = datePicker.querySelector("[data-display-month]") as HTMLButtonElement
 const displayYear = datePicker.querySelector("[data-display-year]") as HTMLButtonElement
-function updateCurrentMonth(dateToShow: Date) {
+function updateDisplayMonthAndYear(dateToShow: Date) {
 	displayMonth.textContent = format(dateToShow, "LLLL")
 	displayYear.textContent = format(dateToShow, "y")
 }
@@ -130,27 +132,33 @@ const monthSelectionGrid = datePicker.querySelector(
 const datePickerCalendarHeader = datePicker.querySelector(
 	"[data-date-picker-grid-header]"
 ) as HTMLDivElement
+function toggleDatePickerGrid(hide: boolean) {
+	datePickerCalendarHeader.classList.toggle("hide", hide)
+	dateGrid.classList.toggle("hide", hide)
+}
+function toggleMonthButtons(disable: boolean) {
+	previousMonthButton.disabled = disable
+	nextMonthButton.disabled = disable
+}
 function toggleMonthSelector() {
 	monthSelectionGrid.classList.toggle("hide")
 	const monthSelectionIsHidden = monthSelectionGrid.classList.contains("hide")
-	datePickerCalendarHeader.classList.toggle("hide", !monthSelectionIsHidden)
-	dateGrid.classList.toggle("hide", !monthSelectionIsHidden)
-	previousMonthButton.disabled = !monthSelectionIsHidden
-	nextMonthButton.disabled = !monthSelectionIsHidden
+	if (!monthSelectionIsHidden) yearSelector.classList.add("hide")
+	toggleDatePickerGrid(!monthSelectionIsHidden)
+	toggleMonthButtons(!monthSelectionIsHidden)
 }
 displayMonth.addEventListener("click", () => toggleMonthSelector())
 
 const monthSelectorButtons = Array.from(
 	datePicker.querySelectorAll("[data-month-selector-button]")
 ) as Array<HTMLButtonElement>
-console.log(monthSelectorButtons)
 monthSelectorButtons.forEach(button => {
 	button.addEventListener("click", () => {
-		const month = Number(button.dataset.month)
+		const month = parseInt(button.dataset.month!)
 		const dateToShow = new Date(datePicker.dataset.dateToShow!)
 		const newDateToShow = setMonth(dateToShow, month)
 		setDateToShow(newDateToShow.toDateString())
-		updateCurrentMonth(newDateToShow)
+		updateDisplayMonthAndYear(newDateToShow)
 		populateDatePicker()
 		toggleMonthSelector()
 	})
@@ -158,6 +166,68 @@ monthSelectorButtons.forEach(button => {
 // 7. BONUS 2: If the year is clicked, bring up a selector that allows the user to select a year, either by
 //      clicking on it in the list of choices or by typing it into the search field. There will be some
 //      limitations on this so that the user can't choose an invalid date, but those can be defined later.
+//		NOTE: I've decided to limit the time window to a 200-year span from 1900 to 2100 for now, but the
+//		start and end years are definable as data properties on the year selector element. If I decide to
+//		make this a completely enclosed reusable component, I should move the properties to the top-level
+//		element of the component, but that's the only change I would need to make from my current setup.
+const yearSelector = datePicker.querySelector("[data-year-selector]") as HTMLDivElement
+function toggleYearSelector() {
+	yearSelector.classList.toggle("hide")
+	const yearSelectorIsHidden = yearSelector.classList.contains("hide")
+	if (!yearSelectorIsHidden) monthSelectionGrid.classList.add("hide")
+	toggleDatePickerGrid(!yearSelectorIsHidden)
+	toggleMonthButtons(!yearSelectorIsHidden)
+	setSelectedAndCurrentYears(yearSelectorIsHidden)
+}
+function setSelectedAndCurrentYears(yearSelectorIsHidden: boolean) {
+	if (yearSelectorIsHidden) return
+	const currentYear = getYear(new Date())
+	const selectedYear = getYear(new Date(datePickerToggle.dataset.selectedDate!))
+	const yearButtons: HTMLButtonElement[] = Array.from(
+		yearSelector.querySelectorAll(".year-selector-button")
+	)
+	for (const yearButton of yearButtons) {
+		const year = parseInt(yearButton.dataset.year!)
+		yearButton.classList.toggle("current-year", year === currentYear)
+		yearButton.classList.toggle("selected-year", year === selectedYear)
+	}
+}
+displayYear.addEventListener("click", () => toggleYearSelector())
+const defaultYears = {
+	start: "1900",
+	end: "2100"
+}
+const startYear = parseInt(yearSelector.dataset.startYear ?? defaultYears.start)
+const endYear = parseInt(yearSelector.dataset.endYear ?? defaultYears.end)
+for (let year = startYear; year <= endYear; year++) {
+	const button = createYearButton(year)
+	yearSelector.appendChild(button)
+}
+function createYearButton(year: number) {
+	const button: HTMLButtonElement = document.createElement("button")
+	button.classList.add("year-selector-button", "selector-button")
+	button.dataset.year = year.toString()
+	button.textContent = year.toString()
+
+	button.addEventListener("click", () => {
+		const year = parseInt(button.dataset.year!)
+		const dateToShow = new Date(datePicker.dataset.dateToShow!)
+		const newDateToShow = setYear(dateToShow, year)
+		setDateToShow(newDateToShow.toDateString())
+		updateDisplayMonthAndYear(newDateToShow)
+		populateDatePicker()
+		toggleYearSelector()
+	})
+
+	return button
+}
+// Tasks remaining for the year selector:
+// - When the selector is first opened, set the scroll position to selected year if one exists. Default to today's date if no date has been selected yet.
+// - Add an input above the list of years that does the following:
+//		- When the input changes, clear any errors and filter the visible year buttons to only show the ones that include the value currently in the input.
+//		- When enter is pressed validate the value. If it is valid, update dateToShow, clear the input, and close the year selector, otherwise, show an error.
+//		- When a year button is clicked, clear the input.
+
 // 8. BONUS 3: Add styling to show today's date when it is visible, but not selected. When the date picker
 //		is first opened, use this styling to show today's date without selecting it.
 // 9. BONUS 4: Add a button that takes the user to today's date.
