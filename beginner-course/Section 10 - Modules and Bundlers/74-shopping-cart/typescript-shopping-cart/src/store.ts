591aebf030a600acc75b648f444c8b11f6a14368
Spencer <spencer.meredith4@gmail.com>
2025-01-17 19:22:24 -0700

    Work for 17 January 2025
    - set up the shopping cart project

---
+++ beginner-course/Section 10 - Modules and Bundlers/74-shopping-cart/typescript-shopping-cart/src/store.ts
@@ -0,0 +1,2 @@
+// 1. Populate the items on the page dynamically. Make the "Add to Cart" button functional when creating each item.
+// BONUS: In the item layout, add a way to change the quantity before adding it to the cart and adjust the "Add to Cart" button to take into account the set quantity. The quantity should default to 1 to keep the same behavior as the "before" implementation.
